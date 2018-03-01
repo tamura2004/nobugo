dee30bbb0863498b8bcb8cca5f6413beaec3b2fb
tamura <tamura2004@gmail.com>
2018-03-01 22:16:21 +0900

    fire

--- usecase/employ.go
+++ usecase/employ.go
@@ -7,14 +7,19 @@ import (
 func employ() {
 	for i := 0; i < 2; i++ {
 		pool := Board[i].Pool
-		color := pool.Winner()
-		pl := Party.GetPlayerByColor(color)
-		card := Board[i].Deck.Draw()
-		pl.Samurai.Bottom(card)
-
 		pretty.Print(pool)
+
+		color := pool.Winner()
 		pretty.Print(color)
+
+		pl := Party.GetPlayerByColor(color)
 		pretty.Print(pl)
+
+		card := Board[i].Deck.Draw()
 		pretty.Print(card)
+
+		pl.Samurai.Bottom(card)
+		pretty.Print(pl)
+
 	}
 }
